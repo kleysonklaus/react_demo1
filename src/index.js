d4377246f93ea001b91d3b8879cc41b48162b643
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-22 22:36:37 -0500

    feat(index.js): first componenet in react

--- src/index.js
+++ src/index.js
@@ -3,4 +3,19 @@ import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<h1>hola asdasd</h1>)
+
+function Greeting() {
+    return <div>
+        <h1>Este es un componente</h1>
+        <p>lorem 123</p>
+    </div>
+}
+
+root.render(<div>
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+</div>)

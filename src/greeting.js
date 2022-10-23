bbbd23b564e6a9665e580f216b11f7b4878f87e5
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-22 23:02:09 -0500

    feat(greeting.js): my first component in react

---
+++ src/greeting.js
@@ -0,0 +1,3 @@
+export function Greeting() {
+    return <h1>Mi primer componente de React</h1>
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { Greeting } from './greeting'
 const root = ReactDOM.createRoot(document.getElementById('root'))
-
-
-function Greeting() {
-    function add(x, y) {
-        return x + y
-    }
-    // return <h1>{user.firstName}</h1>
-    // return <h1>{JSON.stringify(user)}</h1>
-    return <>
-        <h1>{add(10, 30)}</h1>
-        <h1>{add(10, 30)}</h1>
-    </>
-
-}
-
 
 root.render(<>
     <Greeting />

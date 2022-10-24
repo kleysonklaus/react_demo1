f5ad3bd2b6002d1effa9a5c4b978a63f375a011c
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-23 23:03:08 -0500

    feat(props): 2 .- extract title from props

--- src/greeting.js
+++ src/greeting.js
@@ -1,6 +1,6 @@
-export function Greeting(props) {
-    console.log(props)
-    return <h1>Mi primer componente de React : {props.title}</h1>
+export function Greeting({ title }) {
+    // console.log(props)
+    return <h1>Mi primer componente de React : {title}</h1>
 }
 export function UserCard() {
     return <h1>User Card</h1>

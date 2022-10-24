37f8f651ad4ae087edd548a2d51375ce60cbaaf2
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-23 23:21:50 -0500

    feat(props advance): receive many properties in props in component

--- src/greeting.js
+++ src/greeting.js
@@ -1,7 +1,18 @@
 export function Greeting({ title, name = "default value" }) {
-    console.log(title, name)
+    // console.log(title, name)
     return <h1>{title} es igual a = {name}</h1>
 }
-export function UserCard() {
-    return <h1>User Card</h1>
+export function UserCard(props) {
+    console.log(props)
+    return <>
+        <h1>{props.name}</h1>
+        <h1>{props.amount}</h1>
+        <h1>{props.married ? "casado" : "soltero"}</h1>
+        <ul>
+            <li>{props.address.city}</li>
+            <li>{props.address.street}</li>
+        </ul>
+        <h1>{props.points}</h1>
+        <h1>{props.address.toString()}</h1>
+    </>
 }
--- src/index.js
+++ src/index.js
@@ -7,13 +7,19 @@ import Product, { NavBar } from './product'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(<>
-    <Greeting title="hola" name="aguas" />
+    {/* <Greeting title="hola" name="aguas" />
     <Greeting title="kleyson" name="mareas" />
     <Greeting title="klaus" />
     <Greeting title="altura" />
-    <Greeting title="rojo" />
-    {/* <UserCard />
-    <Product />
+    <Greeting title="rojo" /> */}
+    <UserCard
+        name="ryan"
+        amount={3000}
+        married={true}
+        points={[10, 99.9, 12, 45]}
+        address={{ street: "main street", city: "NY" }}
+    />
+    {/* <Product />
     <NavBar /> */}
 </>
 )

2155187d09bcfc737425f7b8cfa9345a8673e832
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-25 22:18:56 -0500

    feat(card_style): extract style in variable

--- src/Task.js
+++ src/Task.js
@@ -1,8 +1,8 @@
 export function TaskCard() {
 
+    const cardStyle = { background: '#202020', color: '#fff', padding: '20px' }
 
-
-    return <div style={{ background: '#202020', color: '#fff', padding: '20px' }}>
+    return <div style={cardStyle}>
         <h1 style={{ fontWeight: 'lighter' }}>Mi primer tarea</h1>
         <p>Tarea realizafa!</p>
     </div>

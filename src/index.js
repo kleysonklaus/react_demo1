ed823f200b0509fd7b175e5da575a996dac62157
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-25 22:57:34 -0500

    faet(Saludar): create component with Class object

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { Greeting, UserCard } from './Greeting'
 import { Button } from './Button'
 import { TaskCard } from './Task'
-
-
 import Product, { NavBar } from './product'
+import { Saludar } from './Saludar'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(<>
 
     <TaskCard ready={false} />
+    <Saludar />
 </>
 )

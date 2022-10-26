f34bf796203f3b9355ca288a448eed9e3b27410a
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-25 22:59:23 -0500

    feat(Saludar): notes with class component

--- src/Saludar.js
+++ src/Saludar.js
@@ -1,5 +1,7 @@
 import { Component } from 'react'
-
+/// Metodo antiguo de hacer componentes - a metodo informativo, no recomendado
+///
+/// es mejor el uso de componentes basados en funciones
 export class Saludar extends Component {
     render() {
         return <h1>Hello world</h1>

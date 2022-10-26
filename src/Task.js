1e13e918b725fb308bf1958fc40f70becf0ec9e9
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-25 22:18:14 -0500

    feat(style component): basic style in component - one line

---
+++ src/Task.js
@@ -0,0 +1,10 @@
+export function TaskCard() {
+
+
+
+    return <div style={{ background: '#202020', color: '#fff', padding: '20px' }}>
+        <h1 style={{ fontWeight: 'lighter' }}>Mi primer tarea</h1>
+        <p>Tarea realizafa!</p>
+    </div>
+
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Greeting, UserCard } from './Greeting'
 import { Button } from './Button'
+import { TaskCard } from './Task'
+
 
 import Product, { NavBar } from './product'
 
@@ -9,25 +11,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(<>
 
-    <Button text="click me" />
-    <Button text="pay" />
-    <Button text="requerido" name="alex" />
-
-    {/* <UserCard
-        name="ryan"
-        amount={3000}
-        married={true}
-        points={[10, 99.9, 12, 45]}
-        address={{ street: "main street", city: "NY" }}
-        greet={function () { alert("hola") }}
-    />
-    <UserCard
-        name="usuario 2"
-        amount={1000}
-        married={false}
-        points={[12, 45]}
-        address={{ street: "av some where", city: "New York" }}
-        greet={function () { alert("hola") }}
-    /> */}
+    <TaskCard />
 </>
 )

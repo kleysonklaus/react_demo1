c4e9d6adcb2923166c14d5a5f84fa0c38f474474
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-25 22:24:00 -0500

    feat(title_style): extract style in title

--- src/Task.js
+++ src/Task.js
@@ -1,9 +1,9 @@
 export function TaskCard() {
 
     const cardStyle = { background: '#202020', color: '#fff', padding: '20px' }
-
+    const titleStyle = { fontWeight: 'lighter' }
     return <div style={cardStyle}>
-        <h1 style={{ fontWeight: 'lighter' }}>Mi primer tarea</h1>
+        <h1 style={titleStyle}>Mi primer tarea</h1>
         <p>Tarea realizafa!</p>
     </div>
 

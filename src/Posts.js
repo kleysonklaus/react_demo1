5db788d9dfd2ecbe073531520b1d32976d967624
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-27 22:52:51 -0500

    feat(Posts): cath error from API

--- src/Posts.js
+++ src/Posts.js
@@ -1,9 +1,11 @@
 export const Posts = () => {
     return <>
         <button onClick={() => {
-            fetch('https://jsonplaceholder.typicode.com/posts')
+            // fetch('https://jsonplaceholder.typicode.com/posts')
+            fetch('https://jsonplaceholder.typicode.xyz/posts')
                 .then(response => response.json())
                 .then(data => console.log(data))
+                .catch(error => console.log(error))
         }}>
             Traer Datos
         </button>

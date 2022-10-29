351aa764da39b20498ea53c21bf036a53d91e0b4
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-28 23:33:57 -0500

    feat(hook - useStaet): with input and show info in alert

--- src/index.js
+++ src/index.js
@@ -7,34 +7,22 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 function Counter() {
 
-    const [counter, setCounter] = useState(0)
+    const [mensaje, setMensaje] = useState('')
 
     return <>
-        <h1>Counter : {counter}</h1>
-        <button onClick={
-            () => {
-                console.log(counter)
-                setCounter(counter + 1)
+        <div>
+            <input onChange={(e) => {
+                setMensaje(e.target.value)
+                console.log(mensaje)
             }
-        }>
-            Sumar
-        </button>
-        <button onClick={
-            () => {
-                console.log(counter)
-                setCounter(counter - 1)
-            }
-        }>
-            Restar
-        </button>
-        <button onClick={
-            () => {
-                console.log(0)
-                setCounter(0)
-            }
-        }>
-            Reiniciar
-        </button>
+
+            } />
+            <button onClick={(e) => {
+                alert("el mensaje es: " + mensaje)
+            }}>
+                Guardar
+            </button>
+        </div>
     </>
 }
 

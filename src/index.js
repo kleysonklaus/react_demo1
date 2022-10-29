8a4d99b6154dc40803d44443d7e75058bc1c0561
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-28 22:35:56 -0500

    feat(hook - useState): not implement, but basic

--- src/index.js
+++ src/index.js
@@ -4,32 +4,14 @@ import { Posts } from './Posts'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const users = [
-    {
-        id: 1,
-        nombre: 'ryan rey',
-        image: 'https://robohash.org/user1'
-    },
-    {
-        id: 2,
-        nombre: 'joe',
-        image: 'https://robohash.org/user2'
-    },
-    {
-        id: 3,
-        nombre: 'joe',
-        image: 'https://robohash.org/user3'
-    }
-]
+
 
 root.render(<>
-    {
-        users.map((user, index) => {
-            return <div key={index}>
-                <h1>{user.nombre}</h1>
-                <img src={user.image} />
-            </div>
-        })
-    }
+    <h1>Counter : 0</h1>
+    <button onClick={
+        console.log("sumar")
+    }>
+        Sumar
+    </button>
 </>
 )

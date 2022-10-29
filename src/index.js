c59b702966bc5972deb9d65cf1023941669ed86f
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-28 22:43:13 -0500

    feat(hook - useState): component to use state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Posts } from './Posts'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 
+function Counter() {
+
+
+    let counter = 10
+    return <>
+        <h1>Counter : {counter}</h1>
+        <button onClick={
+            () => {
+                counter = counter + 1
+                console.log(counter)
+            }
+        }>
+            Sumar
+        </button>
+    </>
+}
 
 root.render(<>
-    <h1>Counter : 0</h1>
-    <button onClick={
-        console.log("sumar")
-    }>
-        Sumar
-    </button>
+
+    <Counter />
 </>
 )
